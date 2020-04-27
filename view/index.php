becffe4de1e03d8040e453253c54955a9444f57e
velixhalim77 <61542240+velixhalim77@users.noreply.github.com>
2020-04-27 21:42:57 +0700

    footer

--- view/index.php
+++ view/index.php
@@ -7,7 +7,7 @@
 
     <!-- css -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-
+    
 </head>
 <body>
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
@@ -70,6 +70,9 @@
             </div>
         </div>
     </div>
+    <nav class="navbar fixed-bottom navbar-dark bg-dark center">
+        <a class="navbar-brand" href="#">RPL - Week12 - Education</a>
+    </nav>
 
     <!-- script -->
     <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
