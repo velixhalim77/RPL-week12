08f262fd015fc6e9830a6778c0e6006348cdcc13
MarcoKeegan <55056594+MarcoKeegan@users.noreply.github.com>
2020-04-30 20:27:42 +0700

    Create page4.php

---
+++ view/page4.php
@@ -0,0 +1,42 @@
+<html>
+<head>
+<title>Biodata</title>
+</head>
+<body>
+<form action="#" style="width: 700px"class="posisi";>
+<fieldset class="h"/>
+<table style="width: 600px;">
+<body style="background-color:pink"> </body>
+
+<tr>
+<td rowspan="10" width="100px">
+<img src="https://i.pinimg.com/originals/a1/95/26/a19526aa1214191e435f67e1bbf6115d.png" width="150px" height="200px" border="5">
+</td>
+</tr>
+<center>
+<font size="25px">
+<b>BIODATA</b>
+</font>
+</center>
+<tr>
+<td><b>NAMA</b></td>
+<td>: Marco Keegan Shandry Harjono</td>
+</tr>
+<tr>
+<td><b>NIM</b></td>
+<td>: 27555</td>
+</tr>
+<tr>
+<td><b>Tempat, Tanggal Lahir</b></td>
+<td>: Cirebon, 02 Oktober 2000</td>
+</tr>
+<tr>
+<td><b>ALAMAT</b></td>
+<td>: Dukuh Semar No.33, Cirebon</td>
+</tr>
+<tr>
+<td><b>JENIS KELAMIN</b></td>
+<td>: Laki - Laki</td>
+</tr>
+</body>
+</html>
