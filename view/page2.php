7e7c753d4af3078fcd1f5f903b6038410be0242f
devinnolanp <49188169+devinnolanp@users.noreply.github.com>
2020-04-30 20:18:58 +0700

    Create page2.php

---
+++ view/page2.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.min.css">
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+<body>
+
+<!-- Navigation -->
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand" href="#">Welcome</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div class="navbar-nav">
+        <a class="nav-item nav-link active" href="#">Home <span class="sr-only">(current)</span></a>
+        <a class="nav-item nav-link" href="view/page1">Page1</a>
+        <a class="nav-item nav-link" href="view/page2">Page2</a>
+        <a class="nav-item nav-link" href="view/page3">Page3</a>
+        <a class="nav-item nav-link" href="view/page4">Page4</a>
+        </div>
+    </div>
+</nav>
+
+<!-- Image -->
+<section>
+  <img class="center" src="gambar/threebroslux.jpg">
+</section>
+
+<!-- Content -->
+<section class="w3-container w3-center w3-content" style="max-width:600px">
+  <h2 class="w3-wide">THREEBROS LUXURY</h2>
+  <p class="w3-opacity"><i>AUTHENTIC LUXURY GOODS ❤️</i></p>
+  <p class="w3-panel">Your Best Luxury Store<br>100% AUTHENTIC GUARANTEED<br>PROVIDE ALL YOU NEED<br>🔸WA: 087825243603 (NoCall)<br>🔹Line: @733pinng<br>💳 Cicilan 0% 3-12 bulan via ⬇️⬇️<br>m.tokopedia.com/threebros</p>
+</section>
+
+
+<!-- Footer -->
+<nav id="footer">
+<footer class="w3-container w3-padding-64 w3-center w3-black w3-xlarge">
+  <a href="https://www.instagram.com/threebros.luxury/"><i class="fa fa-instagram"></i></a>
+  <a href="https://www.facebook.com/threebros.lux"><i class="fa fa-facebook"></i></a>
+</footer>
+</nav>
+
+<style>
+  .center {
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+  width: 20%;
+  }
+</style>
+
+</body>
+</html>
