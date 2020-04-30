0d9bf06a171f2e7270c6c37f1793d0ee29bdcd65
velixhalim77 <61542240+velixhalim77@users.noreply.github.com>
2020-04-30 19:45:11 +0700

    Update page1.php

--- view/page1.php
+++ view/page1.php
@@ -47,6 +47,7 @@
 <nav class="navbar fixed-bottom navbar-dark bg-dark center">
         <a class="navbar-brand" href="#">RPL - Week12 - Education</a>
     </nav>
+    <span>hello world</span>
 
     <!-- script -->
     <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
